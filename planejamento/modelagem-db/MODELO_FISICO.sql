730debba2fb0802cbd0e7c0264bd997c58fa9e27
DESKTOP-CGK9SIM\bruno <bruno.braga@localize.com.br>
2022-05-21 11:28:50 -0300

    chore: ID da tabela agora é o ultimo campo, para facilitar integracao com backend

--- planejamento/modelagem-db/MODELO_FISICO.sql
+++ planejamento/modelagem-db/MODELO_FISICO.sql
@@ -4,13 +4,12 @@ CREATE DATABASE supermercado_sq;
 \c supermercado_sq
 
 
-CREATE TABLE perfil_acesso (
-    cod_perfil_acesso SERIAL PRIMARY KEY,
-    descricao VARCHAR(64) NOT NULL
+CREATE TABLE IF NOT EXISTS perfil_acesso (
+    descricao VARCHAR(64) NOT NULL UNIQUE,
+    cod_perfil_acesso SERIAL PRIMARY KEY
 );
 
-CREATE TABLE usuario (
-    cod_usuario SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS usuario (
     username VARCHAR(64) NOT NULL UNIQUE,
     email VARCHAR(64) NOT NULL UNIQUE,
     senha VARCHAR(64) NOT NULL,
@@ -18,25 +17,26 @@ CREATE TABLE usuario (
     nome VARCHAR(128) NOT NULL,
     documento VARCHAR(32) UNIQUE,
     telefone VARCHAR(32),
-    cod_perfil_acesso INT NOT NULL REFERENCES perfil_acesso(cod_perfil_acesso)    
+    cod_perfil_acesso INT NOT NULL REFERENCES perfil_acesso(cod_perfil_acesso),    
+    cod_usuario SERIAL PRIMARY KEY
 );
 
-CREATE TABLE produto (
-    cod_produto SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS produto (
     nome VARCHAR(64) NOT NULL,
     marca VARCHAR(64),
     ingredientes TEXT NOT NULL,
     img_produto BYTEA,
     img_tabela_nutricional BYTEA,
-    cod_usuario INT NOT NULL REFERENCES usuario(cod_usuario)
+    cod_usuario INT NOT NULL REFERENCES usuario(cod_usuario),
+    cod_produto SERIAL PRIMARY KEY
 );
 
-CREATE TABLE restricao (
-    cod_restricao SERIAL PRIMARY KEY,
-    nome_resticao VARCHAR(64) NOT NULL
+CREATE TABLE IF NOT EXISTS restricao (
+    nome_restricao VARCHAR(64) NOT NULL,
+    cod_restricao SERIAL PRIMARY KEY
 );
 
-CREATE TABLE produto_restricao (
+CREATE TABLE IF NOT EXISTS produto_restricao (
     cod_produto INT NOT NULL REFERENCES produto(cod_produto),
     cod_restricao INT NOT NULL REFERENCES restricao(cod_restricao)
 );
