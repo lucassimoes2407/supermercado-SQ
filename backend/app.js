1c7932ba93e6a768e6d8836a090e71abd9ec4a07
DESKTOP-CGK9SIM\bruno <bruno.braga@localize.com.br>
2022-06-08 19:25:42 -0300

    fix: Correções após ajustes na arquitetura

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const seedRouter = require('./src/routes/seed');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
