3da65e6508c2501f271551147f88f2a5f668e89d
DESKTOP-CGK9SIM\bruno <bruno.braga@localize.com.br>
2022-06-18 17:02:19 -0300

    chore: Pedido da Dreina de alteração do userRoutes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,20 +4,20 @@ const router = express.Router();
 
 //userGets
 router.get('/', userController.getAllUsers);
-router.get('/findByUsername/:username', userController.getUserByUserName);
-router.get('/findByUserId/:id', userController.getUserByUserId);
+router.get('/:username', userController.getUserByUserName);
+router.get('/:id', userController.getUserByUserId);
 router.get('/findUsersActive', userController.getUsersActive);
 router.get('/findUsersInactive', userController.getUsersInactive);
 
 //userPost
-router.post('/createUser', userController.createUser);
+router.post('/', userController.createUser);
 
 //userUpdates
 router.put('/setUserActiveAttribute/:id', userController.setUserActiveAttribute);
-router.put('/updateUser/:id', userController.updateUser);
+router.put('/:id', userController.updateUser);
 
 //userDeletes
-router.delete('/deleteUserByUserName/:username', userController.deleteUserByUserName);
-router.delete('/deleteUserByUserId/:id', userController.deleteUserByUserId);
+router.delete('/:username', userController.deleteUserByUserName);
+router.delete('/:id', userController.deleteUserByUserId);
 
 module.exports = router;
